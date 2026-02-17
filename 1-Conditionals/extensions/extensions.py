26db2fadd9b5632edf6ffb70ece5d86675082aa3
Vladimir G <159206796+Vladimir-Grebenets@users.noreply.github.com>
2026-02-17 02:30:42 +0000

    Completed task 3

--- 1-Conditionals/extensions/extensions.py
+++ 1-Conditionals/extensions/extensions.py
@@ -1,5 +1,19 @@
 files = input("What is your file name? ")
-file = files.casefold()
+file = files.casefold().strip()
 
 if file.endswith(".gif"):
-    print("")
+    print("image/gif")
+elif file.endswith(".jpg"):
+    print("image/jpg")
+elif file.endswith(".jpeg"):
+    print("image/jpeg")
+elif file.endswith(".png"):
+    print("image/png")
+elif file.endswith(".pdf"):
+    print("application/pdf")
+elif file.endswith(".txt"):
+    print("text/plain")
+elif file.endswith(".zip"):
+    print("application/zip")
+else:
+    print("application/octet-stream")
