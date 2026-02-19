377e07df4b59b6a72da87b6aad34ede108e7970e
Vladimir G <159206796+Vladimir-Grebenets@users.noreply.github.com>
2026-02-19 03:06:51 +0000

    completed task 5

--- 1-Conditionals/meal/meal.py
+++ 1-Conditionals/meal/meal.py
@@ -1,14 +1,25 @@
 def main():
     time = input("What is the time? ")
 
-    convert(time)
+    x = convert(time)
 
-    print(time)
+    if x >= 7 and x <= 8:
+        print("Breakfast time")
+    elif x >= 12 and x <= 13:
+        print("Lunch time")
+    elif x >= 18 and x <= 19:
+        print("Dinner time")
+    else:
+        print("Not time for food")
 
 
 def convert(time):
-    time = float(time)
-    time = time / 60
+    hours, mins = time.split(":")
+    hours = float(hours)
+    mins = float(mins)
+    converts = hours + (mins / 60)
+    print(converts)
+    return converts
 
 
 if __name__ == "__main__":
