59fb6a8d48501f3e39684f0603f9980c212b025a
Vladimir G <159206796+Vladimir-Grebenets@users.noreply.github.com>
2026-02-20 00:55:40 +0000

    WIP task 5

---
+++ 2-Loops/nutrition/nutrition.py
@@ -0,0 +1,36 @@
+chosen_fruit = input("Fruit: ").lower()
+
+fruit_dict = [
+    {"fruit": "apple", "Calories": "130"},
+    {"fruit": "avocado", "Calories": "50"},
+    {"fruit": "banana", "Calories": "110"},
+    {"fruit": "cantaloupe", "Calories": "50"},
+    {"fruit": "grapefruit", "Calories": "60"},
+    {"fruit": "grapes", "Calories": "90"},
+    {"fruit": "honeydew melon", "Calories": "50"},
+    {"fruit": "kiwifruit", "Calories": "90"},
+    {"fruit": "lemon", "Calories": "15"},
+    {"fruit": "lime", "Calories": "20"},
+    {"fruit": "nectarine", "Calories": "50"},
+    {"fruit": "orange", "Calories": "80"},
+    {"fruit": "peach", "Calories": "60"},
+    {"fruit": "pear", "Calories": "100"},
+    {"fruit": "pineapple", "Calories": "50"},
+    {"fruit": "plumes", "Calories": "70"},
+    {"fruit": "strawberries", "Calories": "50"},
+    {"fruit": "sweet cherries", "Calories": "100"},
+    {"fruit": "tangerine", "Calories": "50"},
+    {"fruit": "watermelon", "Calories": "80"},
+]
+
+for current_fruit in fruit_dict:
+    if current_fruit["fruit"] == chosen_fruit:
+        print({current_fruit["Calories"]})
+        break
+
+
+"""
+for chosen_fruit in fruit_dict:
+    if fruit_dict["fruit"] == chosen_fruit:
+        print({fruit_dict["fruit"]} has {fruit_dict["Calories"]})
+"""
