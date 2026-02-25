2074568552599a66f7748ba84bb5f0c7d805c30a
Vladimir G <159206796+Vladimir-Grebenets@users.noreply.github.com>
2026-02-25 10:57:14 +0000

    Completed 3.1

--- 3-Exceptions/fuel/fuel.py
+++ 3-Exceptions/fuel/fuel.py
@@ -1,31 +1,31 @@
 def main():
-
     fuel_percent = get_value("What is fuel level? ")
 
-    fuel_converted = calculate(fuel_percent)
+    try:
+        fuel_converted = calculate(fuel_percent)
 
-    if fuel_converted <= 1:
-        print("E")
-    elif fuel_converted >= 1 and fuel_converted < 99:
-        print(fuel_converted)
-    else:
-        print("F")
+        if fuel_converted <= 1:
+            print("E")
+        elif fuel_converted >= 1 and fuel_converted < 99:
+            print(fuel_converted)
+        else:
+            print("F")
+    except (ValueError, ZeroDivisionError):
+        main()
 
 
 def get_value(fuel_percentage):
-
     return input(fuel_percentage)
 
 
 def calculate(fuel_percent):
-    try:
-        x, y = fuel_percent.split("/")
-        x = float(x)
-        y = float(y)
-        fuel_converted = (x / y) * 100
-        return fuel_converted
-    except (ValueError, ZeroDivisionError):
-        pass
+    x, y = fuel_percent.split("/")
+    x = float(x)
+    y = float(y)
+    if x > y:
+        raise ValueError
+    fuel_converted = (x / y) * 100
+    return fuel_converted
 
 
 main()
