6d9f3965a7eeb76f7ddb1f033fd0e55cf38ab33e
Vladimir G <159206796+Vladimir-Grebenets@users.noreply.github.com>
2026-02-28 01:55:58 +0000

    Completed task 3.4

--- 3-Exceptions/outdated/outdated.py
+++ 3-Exceptions/outdated/outdated.py
@@ -2,36 +2,40 @@ def main():
 
     date = input("Enter date in Month/Day/Year or Month Day, Year format: ")
 
-    months_list = [
-        {"January": "01"},
-        {"February": "02"},
-        {"March": "03"},
-        {"April": "04"},
-        {"May": "05"},
-        {"June": "06"},
-        {"July": "07"},
-        {"August": "08"},
-        {"September": "09"},
-        {"October": "10"},
-        {"November": "11"},
-        {"December": "12"},
+    month_list = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
     ]
-
-    month = 2
-    day = 2
 
     try:
         if "/" in date:
             month, day, year = date.split("/")
-            print(f"year + (" - ") + {month:02} + (" - ") + {day:02}")
-
+            if int(day) > 31:
+                raise ValueError
+            if int(month) > 12:
+                raise ValueError
+            print(f"{year}-{month.zfill(2)}-{day.zfill(2)}")
         else:
-            month, day, year = date.split(" " + ", ")
-            print(year, ("-"), {month == months_list}, ("-"), day)
+            clean_date = date.replace(",", "")
+            month, day, year = clean_date.split()
+            if month not in month_list:
+                raise ValueError
+            if int(day) > 31:
+                raise ValueError
+            month = str(month_list.index(month))
+            print(f"{year}-{month.zfill(2)}-{day.zfill(2)}")
     except ValueError:
-        pass
-
-    main()
+        main()
 
 
 main()
