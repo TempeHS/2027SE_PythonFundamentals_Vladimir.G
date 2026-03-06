c8050a7c65bf0b615872fc019c57847a3e4709b3
Vladimir G <159206796+Vladimir-Grebenets@users.noreply.github.com>
2026-03-06 00:55:46 +0000

    Lecture 6 1:25h

---
+++ 6-FileIO/students/students.py
@@ -0,0 +1,22 @@
+import csv
+
+name = input("What is your name")
+home = input("What is your name")
+
+with open("students.csv", "a") as file:
+    writer = csv.DictWriter(file, fieldnames=["name", "home"])
+    writer.writerow({"home": home, "name": name})
+
+"""
+import csv
+
+students = []
+
+with open("students.csv") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        students.append({"name": row["name"], "home": row["home"]})
+
+for student in sorted(students, key=lambda student: student["name"]):
+    print(f"{student["name"]} is in {student["home"]}")
+"""
