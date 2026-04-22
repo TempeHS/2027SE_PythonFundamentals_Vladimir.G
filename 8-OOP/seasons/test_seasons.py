44e19b11db6ce33cc4672bf622e977b47d132083
Vladimir G <159206796+Vladimir-Grebenets@users.noreply.github.com>
2026-04-22 06:09:06 +0000

    Updated pytest for 8.1

--- 8-OOP/seasons/test_seasons.py
+++ 8-OOP/seasons/test_seasons.py
@@ -13,3 +13,10 @@ def test_sub():
     time_between = current_date - past_date
 
     assert time_between == "one thousand, four hundred and forty"
+
+    past_date_1 = Time(datetime(2000, 1, 1))
+    current_date_1 = Time(datetime(2000, 1, 2))
+
+    time_between_1 = current_date_1 - past_date_1
+
+    assert time_between_1 == "five hundred and twenty-five thousand, six hundred"
