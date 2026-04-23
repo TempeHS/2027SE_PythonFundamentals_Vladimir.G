6d9d6867ce0d63ce11c4641f1869dad43fd81e0c
Vladimir G <159206796+Vladimir-Grebenets@users.noreply.github.com>
2026-04-23 11:54:13 +0000

    8.1 finished

--- 8-OOP/seasons/test_seasons.py
+++ 8-OOP/seasons/test_seasons.py
@@ -3,9 +3,6 @@ from seasons import Time
 import seasons
 
 
-def test_past_date(): ...
-
-
 def test_sub():
     past_date = Time(datetime(2000, 1, 1))
     current_date = Time(datetime(2000, 1, 2))
@@ -14,8 +11,8 @@ def test_sub():
 
     assert time_between == "one thousand, four hundred and forty"
 
-    past_date_1 = Time(datetime(2000, 1, 1))
-    current_date_1 = Time(datetime(2000, 1, 2))
+    past_date_1 = Time(datetime(1999, 1, 1))
+    current_date_1 = Time(datetime(2000, 1, 1))
 
     time_between_1 = current_date_1 - past_date_1
 
